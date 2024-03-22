94486d74aa28d57ac6d7c7815c4482eed267c1f7
JackMizh <47821665+JackMizh@users.noreply.github.com>
2024-03-22 19:45:33 +0700

    TL-001: Set-Up Tencent Library

--- app/src/main/java/com/tujuhlive/live/BaseApplication.kt
+++ app/src/main/java/com/tujuhlive/live/BaseApplication.kt
@@ -1,8 +1,18 @@
 package com.tujuhlive.live
 
 import android.app.Application
+import com.tencent.live.TXLiveBase
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val liveLicenceUrl = "https://license.vod-control.com/license/v2/1324250441_1/v_cube.license"
+        val liveKey = "464b08e94f3af0bf630e1c7c86a53364"
+        val ugcLicenceUrl = "https://license.vod-control.com/license/v2/1324250441_1/v_cube.license"
+        val ugcKey = "464b08e94f3af0bf630e1c7c86a53364"
+        TXLiveBase.getInstance().setLicence(this, liveLicenceUrl, liveKey, ugcLicenceUrl, ugcKey)
+    }
 }
--- app/src/main/java/com/tujuhlive/live/MainActivity.kt
+++ app/src/main/java/com/tujuhlive/live/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.tujuhlive.live.navigation.AppNavGraph
@@ -26,6 +24,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             TujuhLiveAppTheme {
                 // A surface container using the 'background' color from the theme
